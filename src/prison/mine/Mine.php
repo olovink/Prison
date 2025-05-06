8f7be3ea1948fa400ced9fed9d2dcae30f3cde17
Etunekov <69630128+olovink@users.noreply.github.com>
2025-05-07 00:21:26 +0700

    hm

--- src/prison/mine/Mine.php
+++ src/prison/mine/Mine.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace prison\mine;
 
+use prison\mine\entry\BlockEntry;
+use prison\mine\entry\MinePosition;
+
 class Mine {
 
+    public function __construct(
+        private MinePosition $minePosition,
+        /** @var BlockEntry[] */
+        private array $blockPool
+    ) {}
 
+    public function getBlockPool(): array{
+        return $this->blockPool;
+    }
 
+    public function getMinePosition(): MinePosition{
+        return $this->minePosition;
+    }
 }
---
+++ src/prison/mine/MineManager.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace prison\mine;
+
+class MineManager {
+
+    public function __construct(
+
+    ) {}
+}
